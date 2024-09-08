d5f05f363aa1c9609464004638b02ed8938d1e38
Feng Shao <88640691+shaofeng66@users.noreply.github.com>
2024-09-08 16:22:33 +0800

    generate none logo kse package (#190)

--- hack/build-kse.sh
+++ hack/build-kse.sh
@@ -1,18 +1,25 @@
 #!/bin/bash -e
 
+export YS1000_VERSION=$(cat charts/ys1000/Chart.yaml | yq '.version')
+
 # customize values.yaml
+if [[ "$1" == "none-logo" ]];then
+    yq eval --inplace ".components.portal.image.tag=\"${YS1000_VERSION}-none-logo\"" charts/ys1000/values.yaml
+fi
 yq eval --inplace '.migconfig.deploymentMode="kse-extension"' charts/ys1000/values.yaml
 yq eval --inplace '.migconfig.deletionPolicy.removeResources=true' charts/ys1000/values.yaml
 yq eval --inplace '.migconfig.deletionPolicy.cancelRunningJobs=true' charts/ys1000/values.yaml
 yq eval --inplace '.mysql.primary.persistence.enabled=true' charts/ys1000/values.yaml
 yq eval --inplace '.mysql.primary.persistence.storageClass=null' charts/ys1000/values.yaml
-export YS1000_VERSION=$(cat charts/ys1000/Chart.yaml | yq '.version')
+
+# customize values.yaml
+if [[ "$1" == "none-logo" ]];then
+    yq eval --inplace ".images |= map(select(.repo == \"mig-ui\").tag=\"${YS1000_VERSION}-none-logo\")" charts/ys1000/images.yaml
+fi
+
 # package ys1000-${YS1000_VERSION}.tgz and move to kse charts dir
 helm package charts/ys1000
 mv ys1000-${YS1000_VERSION}.tgz charts/ys1000-kse/charts/
-# we are safe to restore what we customized
-git restore ys1000-${YS1000_VERSION}.tgz || :
-git restore charts/ys1000/values.yaml
 # update version
 yq -i -e '.version |= env(YS1000_VERSION)' charts/ys1000-kse/extension.yaml
 yq -i -e '.version |= env(YS1000_VERSION)' charts/ys1000-kse/charts/frontend/Chart.yaml
@@ -21,4 +28,15 @@ yq -i -e '.dependencies |= map(.version |= env(YS1000_VERSION))' charts/ys1000-k
 # update images list
 cat charts/ys1000/images.yaml| env TAG_DEFAULT=$(cat charts/ys1000/values.yaml|  yq eval '.imageBase.tag' -r - ) RESITRY_PATH=$(cat charts/ys1000/images.yaml|  yq eval '.ys1000Repo' -r - ) yq eval '.images[] | [env(RESITRY_PATH) + "/" + .repo + ":" + (.tag // env(TAG_DEFAULT))]' | yq -i -e '.images |= (load("/dev/stdin"))' charts/ys1000-kse/extension.yaml
 # build extension
-(cd charts && rm -f ys1000-${YS1000_VERSION}.tgz && ksbuilder package ys1000-kse && mv ys1000-${YS1000_VERSION}.tgz ../ys1000-kse-${YS1000_VERSION}.tgz)
+KSE_PKG_NAME=ys1000-kse-${YS1000_VERSION}.tgz
+if [[ "$1" == "none-logo" ]];then
+    KSE_PKG_NAME=ys1000-kse-${YS1000_VERSION}-none-logo.tgz
+fi
+
+(cd charts && rm -f ys1000-${YS1000_VERSION}.tgz && ksbuilder package ys1000-kse && mv ys1000-${YS1000_VERSION}.tgz ../${KSE_PKG_NAME})
+
+# we are safe to restore what we customized
+rm charts/ys1000-kse/charts/ys1000-${YS1000_VERSION}.tgz
+git restore ys1000-${YS1000_VERSION}.tgz || :
+git restore charts/ys1000/images.yaml
+git restore charts/ys1000/values.yaml
