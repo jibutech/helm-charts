925b516636c1aa65e3c123dbe7876c9cb6bd604b
jerry-jibu <jerry@jibudata.com>
2025-04-16 11:54:45 +0800

    update kse hack script

--- hack/build-kse.sh
+++ hack/build-kse.sh
@@ -3,19 +3,29 @@
 export YS1000_VERSION=$(cat charts/ys1000/Chart.yaml | yq '.version')
 
 # customize values.yaml
-if [[ "$1" == "none-logo" ]];then
-    yq eval --inplace ".components.portal.image.tag=\"${YS1000_VERSION}-none-logo\"" charts/ys1000/values.yaml
+# $1 is vendor
+# $2 is ui tag
+if [[ ! -z "$1" ]];then
+    if [[ ! -z "$2" ]];then
+        yq eval --inplace ".components.portal.image.tag=\"$2\"" charts/ys1000/values.yaml
+    else 
+        yq eval --inplace ".components.portal.image.tag=\"${YS1000_VERSION}-${1}\"" charts/ys1000/values.yaml
+    fi
 fi
 yq eval --inplace '.migconfig.deploymentMode="kse-extension"' charts/ys1000/values.yaml
-yq eval --inplace '.migconfig.deletionPolicy.removeResources=false' charts/ys1000/values.yaml
+# yq eval --inplace '.migconfig.deletionPolicy.removeResources=true' charts/ys1000/values.yaml
 yq eval --inplace '.migconfig.deletionPolicy.cancelRunningJobs=true' charts/ys1000/values.yaml
 yq eval --inplace '.mysql.primary.persistence.enabled=true' charts/ys1000/values.yaml
 yq eval --inplace '.mysql.primary.persistence.storageClass=null' charts/ys1000/values.yaml
 yq eval --inplace '.featureGates.Tenant=true' charts/ys1000/values.yaml
 
 # customize values.yaml
-if [[ "$1" == "none-logo" ]];then
-    yq eval --inplace ".images |= map(select(.repo == \"mig-ui\").tag=\"${YS1000_VERSION}-none-logo\")" charts/ys1000/images.yaml
+if [[ ! -z "$1" ]];then
+    if [[ ! -z "$2" ]];then
+        yq eval --inplace ".images |= map(select(.repo == \"mig-ui\").tag=\"$2\")" charts/ys1000/images.yaml
+    else
+        yq eval --inplace ".images |= map(select(.repo == \"mig-ui\").tag=\"${YS1000_VERSION}-${1}\")" charts/ys1000/images.yaml
+    fi
 fi
 
 # package ys1000-${YS1000_VERSION}.tgz and move to kse charts dir
@@ -30,8 +40,8 @@ yq -i -e '.dependencies |= map(.version |= env(YS1000_VERSION))' charts/ys1000-k
 cat charts/ys1000/images.yaml| env TAG_DEFAULT=$(cat charts/ys1000/values.yaml|  yq eval '.imageBase.tag' -r - ) RESITRY_PATH=$(cat charts/ys1000/images.yaml|  yq eval '.ys1000Repo' -r - ) yq eval '.images[] | [env(RESITRY_PATH) + "/" + .repo + ":" + (.tag // env(TAG_DEFAULT))]' | yq -i -e '.images |= (load("/dev/stdin"))' charts/ys1000-kse/extension.yaml
 # build extension
 KSE_PKG_NAME=ys1000-kse-${YS1000_VERSION}.tgz
-if [[ "$1" == "none-logo" ]];then
-    KSE_PKG_NAME=ys1000-kse-${YS1000_VERSION}-none-logo.tgz
+if [[ ! -z "$1" ]];then
+    KSE_PKG_NAME=ys1000-kse-${YS1000_VERSION}-${1}.tgz
 fi
 
 (cd charts && rm -f ys1000-${YS1000_VERSION}.tgz && ksbuilder package ys1000-kse && mv ys1000-${YS1000_VERSION}.tgz ../${KSE_PKG_NAME})
